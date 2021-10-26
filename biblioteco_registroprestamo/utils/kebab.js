1ddf694e53c6491084fdd26b08e145aa07f46154
Javier M. A <67672011+javim-cryto@users.noreply.github.com>
2021-10-26 13:13:34 -0500

    Delete biblioteco_registroprestamo directory

--- biblioteco_registroprestamo/utils/kebab.js
+++
@@ -1,7 +0,0 @@
-const kebabcase = (str) =>
-  str
-    .replace(/([a-z])([A-Z])/g, '$1-$2') // get all lowercase letters that are near to uppercase ones
-    .replace(/[\s_]+/g, '-') // replace all spaces and low dash
-    .toLowerCase(); // convert to lower case
-
-module.exports = kebabcase;
